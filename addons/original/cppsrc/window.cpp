b2eef9f0719979ca482478fdcbc4804bbf1eb3aa
Chase Lambert <theicfire@gmail.com>
2020-01-01 14:25:10 -0800

    Refactor

--- addons/original/cppsrc/window.cpp
+++ addons/original/cppsrc/window.cpp
@@ -62,25 +62,225 @@ static bool is_hw_decoded(AVFrame* frame) {
     return frame->format == AV_PIX_FMT_VIDEOTOOLBOX;
 }
 
+class SDLHolder {
+    SDL_Texture *texture;
+    SDL_Renderer *renderer;
+    SDL_Window *window;
+    Uint8 *yPlane, *uPlane, *vPlane;
+    SwsContext *sws_ctx = nullptr;
+
+    public:
+void sdl_init(int width, int height, enum AVPixelFormat pix_fmt) {
+
+    size_t yPlaneSz, uvPlaneSz;
+    // Initial SDL subsystems
+
+    if (SDL_Init(SDL_INIT_VIDEO)) {
+        fprintf(stderr, "Could not initialize SDL - %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    window = SDL_CreateWindow(
+            "Videotoolbox Decoder" /* title */,
+            SDL_WINDOWPOS_CENTERED /* x */,
+            SDL_WINDOWPOS_CENTERED /* y */,
+            width,
+            height,
+            SDL_WINDOW_SHOWN
+    );
+
+    if (!window) {
+        std::cerr << "SDL could not create a window." << std::endl;
+        exit(1);
+    }
+
+    // Make a renderer that is to render to the screen
+
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    if (!renderer) {
+        std::cerr << "SDL could not create a renderer." << std::endl;
+        exit(1);
+    }
+
+    // Allocate a place to put YUV image on the screen
+
+    texture = SDL_CreateTexture(
+            renderer,
+            SDL_PIXELFORMAT_YV12,
+            SDL_TEXTUREACCESS_STREAMING,
+            width,
+            height
+    );
+    if (!texture) {
+        std::cerr << "SDL could not create a texture." << std::endl;
+        exit(1);
+    }
+
+    // Initialize SWS context for software scaling
+
+    sws_ctx = sws_getContext(
+            width,
+            height,
+            pix_fmt,
+            width,
+            height,
+            AV_PIX_FMT_YUV420P,
+            SWS_BILINEAR,
+            nullptr,
+            nullptr,
+            nullptr
+    );
+
+    // set up YV12 pixel array (12 bits per pixel)
+
+    yPlaneSz = width * height;
+    uvPlaneSz = width * height / 4;
+    yPlane = (Uint8*)malloc(yPlaneSz);
+    uPlane = (Uint8*)malloc(uvPlaneSz);
+    vPlane = (Uint8*)malloc(uvPlaneSz);
+
+    if (!yPlane || !uPlane || !vPlane) {
+        fprintf(stderr, "Could not allocate pixel buffers - exiting\n");
+        exit(1);
+    }
+
+
+}
+
+void sdl_render(AVFrame* frame, int width, int height) {
+    int uvPitch = width / 2;
+    if (is_hw_decoded(frame)) {
+
+        CVPixelBufferRef ref = (CVPixelBufferRef) frame->data[3];
+
+        CVPixelBufferLockBaseAddress(ref, 0);
+
+        // debug stuff
+
+        // looking at pixel format of CVPixelBuffer
+        // this should be same as what is set in hw_surface_fmt
+        // OSType type = CVPixelBufferGetPixelFormatType(ref);
+        // std::cout << "Format of CVPixelBuffer: " << (type == kCVPixelFormatType_420YpCbCr8Planar) << ", frame format: " << AV_PIX_FMT_GBRAP10LE << std::endl;
+        // frame->format == AV_PIX_FMT_VIDEOTOOLBOX
+
+        uint8_t *yDestPlane = (uint8_t*) CVPixelBufferGetBaseAddressOfPlane(ref, 0);
+        uint8_t *uDestPlane = (uint8_t*) CVPixelBufferGetBaseAddressOfPlane(ref, 1);
+        uint8_t *vDestPlane = (uint8_t*) CVPixelBufferGetBaseAddressOfPlane(ref, 2);
+
+        SDL_UpdateYUVTexture(
+                texture,
+                nullptr,
+                yDestPlane,
+                CVPixelBufferGetBytesPerRowOfPlane(ref, 0),
+                uDestPlane,
+                CVPixelBufferGetBytesPerRowOfPlane(ref, 1),
+                vDestPlane,
+                CVPixelBufferGetBytesPerRowOfPlane(ref, 2)
+        );
+
+        // Clear rendering target
+
+        SDL_RenderClear(renderer);
+
+        // Copy portion of texture to rendering target
+
+        SDL_RenderCopy(
+                renderer,
+                texture,
+                nullptr /* copy entire texture */,
+                nullptr /* entire rendering target */
+        );
+
+        // The other Render* methods draw to hidden target
+        // This function actually draws to window tied to renderer
+
+        SDL_RenderPresent(renderer);
+
+        CVPixelBufferUnlockBaseAddress(ref, 0);
+
+    } else {
+
+        AVPicture pict;
+        pict.data[0] = yPlane;
+        pict.data[1] = uPlane;
+        pict.data[2] = vPlane;
+        pict.linesize[0] = width;
+        pict.linesize[1] = uvPitch;
+        pict.linesize[2] = uvPitch;
+
+        // Convert the image into YUV format for SDL
+
+        sws_scale(
+                sws_ctx,
+                (uint8_t const * const *) frame->data,
+                frame->linesize,
+                0,
+                height,
+                pict.data,
+                pict.linesize);
+
+        SDL_UpdateYUVTexture(
+                texture,
+                NULL,
+                yPlane,
+                width,
+                uPlane,
+                uvPitch,
+                vPlane,
+                uvPitch);
+
+        // Clear rendering target
+
+        SDL_RenderClear(renderer);
+
+        // Copy portion of texture to rendering target
+
+        SDL_RenderCopy(
+                renderer,
+                texture,
+                nullptr /* copy entire texture */,
+                nullptr /* entire rendering target */
+        );
+
+        // The other Render* methods draw to hidden target
+        // This function actually draws to window tied to renderer
+
+        SDL_RenderPresent(renderer);
+
+    }
+}
+
+void sdl_handle_events() {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+    switch (event.type) {
+        case SDL_QUIT:
+            SDL_DestroyTexture(texture);
+            SDL_DestroyRenderer(renderer);
+            SDL_DestroyWindow(window);
+            SDL_Quit();
+            exit(0);
+            break;
+        default:
+            break;
+    }
+}
+
+};
+
+
 int run_program(std::string filename) {
 
     clock_t start = clock();
 
-    int ret, videoStream, uvPitch;
+    int ret, videoStream;
     AVFormatContext *pFormatCtx = nullptr;
     AVCodecContext *decoderCtx = nullptr;
     AVCodec *decoder = nullptr;
     AVStream *video = nullptr;
     AVFrame *frame = nullptr;
     AVPacket* packet = av_packet_alloc();
-    SwsContext *sws_ctx = nullptr;
-    Uint8 *yPlane, *uPlane, *vPlane;
-    size_t yPlaneSz, uvPlaneSz;
-
-    SDL_Event event;
-    SDL_Window *window;
-    SDL_Renderer *renderer;
-    SDL_Texture *texture;
+    SDLHolder sdl_stuff;
 
     // Validate filepath is present
 
@@ -88,12 +288,6 @@ int run_program(std::string filename) {
 
     av_register_all();
 
-    // Initial SDL subsystems
-
-    if (SDL_Init(SDL_INIT_VIDEO)) {
-        fprintf(stderr, "Could not initialize SDL - %s\n", SDL_GetError());
-        exit(1);
-    }
 
     // Open video file
 
@@ -146,219 +340,32 @@ int run_program(std::string filename) {
     }
 
     frame = av_frame_alloc();
-
-    window = SDL_CreateWindow(
-            "Videotoolbox Decoder" /* title */,
-            SDL_WINDOWPOS_CENTERED /* x */,
-            SDL_WINDOWPOS_CENTERED /* y */,
-            decoderCtx->width,
-            decoderCtx->height,
-            SDL_WINDOW_SHOWN
-    );
-
-    if (!window) {
-        std::cerr << "SDL could not create a window." << std::endl;
-        exit(1);
-    }
-
-    // Make a renderer that is to render to the screen
-
-    renderer = SDL_CreateRenderer(window, -1, 0);
-    if (!renderer) {
-        std::cerr << "SDL could not create a renderer." << std::endl;
-        exit(1);
-    }
-
-    // Allocate a place to put YUV image on the screen
-
-    texture = SDL_CreateTexture(
-            renderer,
-            SDL_PIXELFORMAT_YV12,
-            SDL_TEXTUREACCESS_STREAMING,
-            decoderCtx->width,
-            decoderCtx->height
-    );
-    if (!texture) {
-        std::cerr << "SDL could not create a texture." << std::endl;
-        exit(1);
-    }
-
-    // Initialize SWS context for software scaling
-
-    sws_ctx = sws_getContext(
-            decoderCtx->width,
-            decoderCtx->height,
-            decoderCtx->pix_fmt,
-            decoderCtx->width,
-            decoderCtx->height,
-            AV_PIX_FMT_YUV420P,
-            SWS_BILINEAR,
-            nullptr,
-            nullptr,
-            nullptr
-    );
-
-    // set up YV12 pixel array (12 bits per pixel)
-
-    yPlaneSz = decoderCtx->width * decoderCtx->height;
-    uvPlaneSz = decoderCtx->width * decoderCtx->height / 4;
-    yPlane = (Uint8*)malloc(yPlaneSz);
-    uPlane = (Uint8*)malloc(uvPlaneSz);
-    vPlane = (Uint8*)malloc(uvPlaneSz);
-
-    if (!yPlane || !uPlane || !vPlane) {
-        fprintf(stderr, "Could not allocate pixel buffers - exiting\n");
-        exit(1);
-    }
-
-    uvPitch = decoderCtx->width / 2;
+    sdl_stuff.sdl_init(decoderCtx->width, decoderCtx->height, decoderCtx->pix_fmt);
 
     // Splits what is stored in the file into frames and returns one each call.
     // These frames have not been decoded yet.
-
     while(av_read_frame(pFormatCtx, packet) == 0) {
-
         if (packet->stream_index == videoStream) {
-
-
             // Supply raw packet data as input to the decoder.
             ret = avcodec_send_packet(decoderCtx, packet);
 
             int result = avcodec_receive_frame(decoderCtx, frame);
             if (result == 0) {
-
-                if (is_hw_decoded(frame)) {
-
-                    CVPixelBufferRef ref = (CVPixelBufferRef) frame->data[3];
-
-                    CVPixelBufferLockBaseAddress(ref, 0);
-
-                    // debug stuff
-
-                    // looking at pixel format of CVPixelBuffer
-                    // this should be same as what is set in hw_surface_fmt
-                    // OSType type = CVPixelBufferGetPixelFormatType(ref);
-                    // std::cout << "Format of CVPixelBuffer: " << (type == kCVPixelFormatType_420YpCbCr8Planar) << ", frame format: " << AV_PIX_FMT_GBRAP10LE << std::endl;
-                    // frame->format == AV_PIX_FMT_VIDEOTOOLBOX
-
-                    uint8_t *yDestPlane = (uint8_t*) CVPixelBufferGetBaseAddressOfPlane(ref, 0);
-                    uint8_t *uDestPlane = (uint8_t*) CVPixelBufferGetBaseAddressOfPlane(ref, 1);
-                    uint8_t *vDestPlane = (uint8_t*) CVPixelBufferGetBaseAddressOfPlane(ref, 2);
-
-                    SDL_UpdateYUVTexture(
-                            texture,
-                            nullptr,
-                            yDestPlane,
-                            CVPixelBufferGetBytesPerRowOfPlane(ref, 0),
-                            uDestPlane,
-                            CVPixelBufferGetBytesPerRowOfPlane(ref, 1),
-                            vDestPlane,
-                            CVPixelBufferGetBytesPerRowOfPlane(ref, 2)
-                    );
-
-                    // Clear rendering target
-
-                    SDL_RenderClear(renderer);
-
-                    // Copy portion of texture to rendering target
-
-                    SDL_RenderCopy(
-                            renderer,
-                            texture,
-                            nullptr /* copy entire texture */,
-                            nullptr /* entire rendering target */
-                    );
-
-                    // The other Render* methods draw to hidden target
-                    // This function actually draws to window tied to renderer
-
-                    SDL_RenderPresent(renderer);
-
-                    CVPixelBufferUnlockBaseAddress(ref, 0);
-
-                } else {
-
-                    AVPicture pict;
-                    pict.data[0] = yPlane;
-                    pict.data[1] = uPlane;
-                    pict.data[2] = vPlane;
-                    pict.linesize[0] = decoderCtx->width;
-                    pict.linesize[1] = uvPitch;
-                    pict.linesize[2] = uvPitch;
-
-                    // Convert the image into YUV format for SDL
-
-                    sws_scale(
-                            sws_ctx,
-                            (uint8_t const * const *) frame->data,
-                            frame->linesize,
-                            0,
-                            decoderCtx->height,
-                            pict.data,
-                            pict.linesize);
-
-                    SDL_UpdateYUVTexture(
-                            texture,
-                            NULL,
-                            yPlane,
-                            decoderCtx->width,
-                            uPlane,
-                            uvPitch,
-                            vPlane,
-                            uvPitch);
-
-                    // Clear rendering target
-
-                    SDL_RenderClear(renderer);
-
-                    // Copy portion of texture to rendering target
-
-                    SDL_RenderCopy(
-                            renderer,
-                            texture,
-                            nullptr /* copy entire texture */,
-                            nullptr /* entire rendering target */
-                    );
-
-                    // The other Render* methods draw to hidden target
-                    // This function actually draws to window tied to renderer
-
-                    SDL_RenderPresent(renderer);
-
-                }
-
-
+                sdl_stuff.sdl_render(frame, decoderCtx->width, decoderCtx->height);
             } else if (result == AVERROR(EINVAL)) {
-
                 std::cerr << "Codec not opened, or it is an encoder." << std::endl;
                 exit(1);
-
             } else if (result == AVERROR_EOF) {
-
                 std::cerr << "End of file reached" << std::endl;
-
             } else if (result == AVERROR(EAGAIN)) {
-
                 std::cerr << "Output is not available in this state - send more input to decoder." << std::endl;
-
             }
-
+        } else {
+            printf("Got a stream that was not a video\n");
         }
 
         av_packet_unref(packet);
-
-        SDL_PollEvent(&event);
-        switch (event.type) {
-            case SDL_QUIT:
-                SDL_DestroyTexture(texture);
-                SDL_DestroyRenderer(renderer);
-                SDL_DestroyWindow(window);
-                SDL_Quit();
-                exit(0);
-                break;
-            default:
-                break;
-        }
+        sdl_stuff.sdl_handle_events();
     }
 
     if (decoderCtx->hwaccel != nullptr) {
