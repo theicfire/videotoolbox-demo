83a38b14148d25311d86f696b5e94338c4c8a790
Chase Lambert <theicfire@gmail.com>
2020-04-02 17:02:17 -0700

    Add ability to run both original and fast

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const addon = require('./addons/fast/addon.node');
+function run_original() {
+	const addon = require('./addons/original/addon.node');
+	addon.start_client('hello.h264');
+}
+
+function run_fast() {
+	const addon = require('./addons/fast/addon.node');
+	addon.start_client('frames');
+}
 
 console.log('Starting client');
-addon.start_client("frames");
+run_fast();
